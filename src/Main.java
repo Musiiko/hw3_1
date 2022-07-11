88742866b4cdbfa8da0b0e34fa91e2a53f53083e
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-07-12 02:12:24 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arrayN = {0, 2, 4, 6, 3, -1, 1};
+        int[] arrayK = {4, 6, 3};
+        boolean isSubsequence = false;
+
+        for (int i = 0; i < arrayN.length; i++) {
+            for (int j = 0; j < arrayK.length; j++) {
+                if ((i + j) >= arrayN.length) {
+                    break;
+                }
+                if (arrayN[i + j] == arrayK[j]) {
+                    isSubsequence = true;
+                } else {
+                    isSubsequence = false;
+                    break;
+                }
+            }
+            if (isSubsequence) {
+                System.out.println("The second sequence K is included in the first N.");
+                break;
+            }
+        }
+        if (!isSubsequence) {
+            System.out.println("The second sequence K is not included in the first N.");
+        }
     }
 }
